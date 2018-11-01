67312d3d0ab13e9f2c04b2f6f927b61687d98692
Nicole Li <li.nicole160@gmail.com>
2018-11-01 19:05:35 -0400

    fix merge

--- components/Compass.js
+++ components/Compass.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Image, Animated, Easing, Dimensions} from 'react-native';
 import { Location, Permissions } from 'expo';
+const deviceHeight = 87
+const deviceWidth = 43.5
 const proportions={
+  height:58 * 4,
+  width: 28 * 4,
   animated:{
-    width: 27,
-    height:28.5,
-    left:7.5,
-    top:29.25,
+    width:  deviceWidth  - 10,
+    height:deviceHeight/2 - 10,
+    left:deviceWidth /2 -  (deviceWidth   - 10)/2,
+    top:deviceHeight /2 - (deviceHeight/2  - 10)/2,
   },
-  transform: -23.375,
+  transform: -(deviceHeight/2 - (deviceHeight/2  - 10)/2) - 50,
   arrow:{
-    width:6,
-    height:6,
-    left: 6,
-    top: 27
+    width: deviceWidth/7,
+    height:deviceWidth/7,
+    left: deviceWidth /2 - (deviceWidth/7)/2,
+    top: deviceHeight /2  - (deviceWidth/7)/2,
   }
-
-
-
-
 }
 export default class Compass extends Component {
   constructor() {
@@ -31,15 +31,12 @@ export default class Compass extends Component {
         truenoth: null
       };
   }
-
   componentWillMount() {
     this._getLocationAsync();
   }
-
   componentWillUpdate() {
     this.spin()
   }
-
   _getLocationAsync = async () => {
     // Checking device location permissions
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
@@ -56,21 +53,16 @@ export default class Compass extends Component {
       })
     }
   };
-
   spin() {
     let start = JSON.stringify(this.spinValue);
     let heading =  Math.round(this.state.heading);
-
     let rot  = +start;
     let rotM = rot % 360;
-
     if(rotM < 180 && (heading > (rotM + 180)))
       rot -= 360;
     if(rotM >= 180 && (heading <= (rotM - 180)))
       rot += 360
-
     rot += (heading - rotM)
-
     Animated.timing(
       this.spinValue,
       {
@@ -80,54 +72,41 @@ export default class Compass extends Component {
       }
     ).start()
   }
-
   render() {
     let LoadingText = 'Loading...';
     let display = LoadingText;
-
     if (this.state.errorMessage)
       display = this.state.errorMessage;
-
     const spin = this.spinValue.interpolate({
       inputRange: [0,360],
       outputRange: ['-0deg', '-360deg']
     })
-
     display = Math.round(JSON.stringify(this.spinValue))
-
     if(display < 0)
       display += 360
     if(display > 360)
       display -= 360
-
-
     return (
       <View style={styles.container}>
-        <View style={{marginBottom:-12.5, marginRight:-16.2}}>
+        <View style={{marginBottom:-95, marginRight:-12.5}}>
         <Text style={styles.text}>{display+'°'}</Text>
       </View>
-
-        <View style={[styles.imageContainer,{marginTop:-5}]} >
+        <View style={[styles.imageContainer,{marginTop:-6.5}]} >
           <Animated.Image resizeMode='contain' source={require('../assets/compass.png')}
             style={{
-            width:  27, height: 28.5,
-            left: 7.5, top:  29.25,
+            width:  proportions.animated.width, height:proportions.animated.height,
+            left: proportions.animated.left, top: proportions.animated.top,
             transform: [{rotate: spin}],
           }} />
         </View>
-
-        <View style={[styles.arrowContainer,{marginTop:-5}]} >
+        <View style={[styles.arrowContainer,{marginTop:-6.5}]} >
           <Image resizeMode='contain' source={require('../assets/arrow.png')} style={styles.arrow} />
         </View>
-
       </View>
     );
   }
 }
-
 // Device dimensions so we can properly center the images set to 'position: absolute'
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -136,11 +115,11 @@ const styles = StyleSheet.create({
     backgroundColor:'#FDFAEB'
   },
   text: {
-    color: 'black',
-    fontSize: 8,
-    transform: ([{translateY:-23.375 }])
+    color: 'red',
+    fontSize: 14,
+    fontWeight: 'bold',
+    transform: ([{translateY: proportions.transform }])
   },
-
   imageContainer: {
     ...StyleSheet.absoluteFillObject,
   },
@@ -148,10 +127,10 @@ const styles = StyleSheet.create({
   ...StyleSheet.absoluteFillObject,
   },
   arrow: {
-    width: 6,
-    height: 6,
-    left: 18,
-    top: 27,
+    width: proportions.arrow.width,
+    height: proportions.arrow.height,
+    left: proportions.arrow.left,
+    top: proportions.arrow.top,
     opacity: 0.9
   }
 });
